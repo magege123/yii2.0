f5167d2209c7de215c8f1499e0a007ae38a2ecc3
majc <majc@51cto.com>
2017-07-20 18:15:20 +0800

    加入

--- config/params.php
+++ config/params.php
@@ -5,5 +5,11 @@ return [
         'web'=>'http://book.imooc.test/web',
         'm'=>'http://book.imooc.test/m',
         'www'=>'http://book.imooc.test',
+    ],
+
+    'uploads'=>[
+        'avatar'=>'/uploads/avatar/',
+        'book'=>'/uploads/book/',
+        'brand'=>'/uploads/brand/',
     ]
 ];

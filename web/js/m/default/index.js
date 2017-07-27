7dd0b0e654ca50953f584294293d18199fcdbac5
影子 <895309664@qq.com>
2017-07-27 21:22:32 +0800

    新添加文件

---
+++ web/js/m/default/index.js
@@ -0,0 +1,11 @@
+;
+$(function () {
+    TouchSlide({
+        slideCell:"#slideBox",
+        titCell:'.hd ul',
+        mainCell:'.bd ul',
+        autoPage:true,
+        autoPlay:true,
+        effect:"leftLoop"
+    });
+})
